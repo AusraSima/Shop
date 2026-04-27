627007accb55e354d0a5afff22aea0c82217d61d
AusraSima <ausrsima@yahoo.com>
2026-04-27 10:49:19 +0300

    Added EF Core, connected DB

--- Shop.Console/Program.cs
+++ Shop.Console/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shop.Data;
 using Shop.Entities;
 using Shop.Repositories;
 using Shop.Services;
@@ -9,43 +13,66 @@ namespace Shop.ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			var collection = new ServiceCollection();
+			var host = BuildHost();
 
-			collection.AddScoped<IProductRepository, ProductRepository>();
-			collection.AddScoped<IProductService, ProductService>();
-			collection.AddScoped<IBasketRepository, BasketRepository>();
-			collection.AddScoped<IBasketService, BasketService>();
-
-			var serviceProvider = collection.BuildServiceProvider();
-
-			var productService = serviceProvider.GetRequiredService<IProductService>();
-			var basketService = serviceProvider.GetRequiredService<IBasketService>();
-
-			productService.Add(new Product()
+			using (var scope = host.Services.CreateScope())
 			{
-				Name = "Book",
-				Price = 1.99M
-			});
-			productService.Add(new Product()
+				var serviceProvider = scope.ServiceProvider;
+
+				var dbContext = serviceProvider.GetRequiredService<ShopDbContext>();
+				dbContext.Database.Migrate();
+
+				var productService = serviceProvider.GetRequiredService<IProductService>();
+				var basketService = serviceProvider.GetRequiredService<IBasketService>();
+
+				productService.Add(new Product()
+				{
+					Name = "Book",
+					Price = 1.99M
+				});
+				productService.Add(new Product()
+				{
+					Name = "Pen",
+					Price = 1.29M
+				});
+
+				var product = productService.Get(1);
+				Console.WriteLine($"id: {product.Id}; name: {product.Name}");
+				product = productService.Get(2);
+				Console.WriteLine($"id: {product.Id}; name: {product.Name}");
+			}
+
+
+
+			//basketService.Add(1, 1, 2);
+			//basketService.Add(2, 2, 3);
+			//var basket = basketService.Get(1);
+			//Console.WriteLine($"userId: {basket.UserId}; productId: {basket.Products[0].ProductId}; count: {basket.Products[0].Count}");
+			//basket = basketService.Get(2);
+			//Console.WriteLine($"userId: {basket.UserId}; productId: {basket.Products[0].ProductId}; count: {basket.Products[0].Count}");
+			//basketService.Add(1, 2, 3);
+			//basketService.Add(2, 2, 5);
+
+		}
+
+		public static IHost BuildHost()
+		{
+			var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
 			{
-				Name = "Pen",
-				Price = 1.29M
+				var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+
+				services.AddDbContext<ShopDbContext>(options =>
+				{
+					options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+				});
+
+				services.AddScoped<IProductRepository, ProductRepository>();
+				services.AddScoped<IProductService, ProductService>();
+				services.AddScoped<IBasketRepository, BasketRepository>();
+				services.AddScoped<IBasketService, BasketService>();
 			});
 
-			var product = productService.Get(1);
-			Console.WriteLine($"id: {product.Id}; name: {product.Name}");
-			product = productService.Get(2);
-			Console.WriteLine($"id: {product.Id}; name: {product.Name}");
-
-			basketService.Add(1, 1, 2);
-			basketService.Add(2, 2, 3);
-			var basket = basketService.Get(1);
-			Console.WriteLine($"userId: {basket.UserId}; productId: {basket.Products[0].ProductId}; count: {basket.Products[0].Count}");
-			basket = basketService.Get(2);
-			Console.WriteLine($"userId: {basket.UserId}; productId: {basket.Products[0].ProductId}; count: {basket.Products[0].Count}");
-			basketService.Add(1, 2, 3);
-			basketService.Add(2, 2, 5);
-
+			return host.Build();
 		}
 	}
 }
--- Shop.Data/ShopDbContext.cs
+++ Shop.Data/ShopDbContext.cs
@@ -6,16 +6,22 @@ namespace Shop.Data
 	public class ShopDbContext : DbContext
 	{
 		public DbSet<Product> Products { get; set; }
+
+		public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+		{
+		}
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+
 			base.OnModelCreating(modelBuilder);
 
 			modelBuilder.Entity<Product>(o =>
-			{
-				o.HasKey(e => e.Id);
-				o.Property(e => e.Name).IsRequired();
-				o.Property(e => e.Price).IsRequired();
-			});
+				{
+					o.HasKey(e => e.Id);
+					o.Property(e => e.Name).IsRequired();
+					o.Property(e => e.Price).IsRequired();
+				});
 		}
 	}
 }
